54f69c01756165a460d2e73e8e8fdf24416f3394
laurentia25 <lvasilasco25@gmail.com>
2023-11-30 17:54:44 +0200

    Data preprocessing tools

--- 1_Data_preprocessing/data_preprocessing_tools.py
+++ 1_Data_preprocessing/data_preprocessing_tools.py
@@ -10,7 +10,7 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values # in order to take all row data -> [ : ,  all the columns except the last one -> :-1]
 y = dataset.iloc[:, -1].values # all th rows and the last column
-print(dataset)
+
 # Taking care of missing data
 # how to identify missing data
 missing_data = dataset.isnull().sum()
@@ -24,17 +24,34 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
 
+# Encoding the Independent Variable
+
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
+print(X)
 
-# Encoding the Independent Variable
+# Encoding the Dependent Variable
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-
-# Encoding the Dependent Variable
 # Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
 # Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train[:, 3:] = scaler.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = scaler.transform(X_test[:, 3:]) #new data transformed with the same scaler obtained during training machine
+
+print(X_train)
+print(X_test)
+
