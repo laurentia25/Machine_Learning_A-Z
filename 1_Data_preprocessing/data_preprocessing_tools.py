8b1c94cd1e5ebb96937a214b3d696ce6340c3375
laurentia25 <lvasilasco25@gmail.com>
2023-11-30 14:51:10 +0200

    Taking care of missing data

--- 1_Data_preprocessing/data_preprocessing_tools.py
+++ 1_Data_preprocessing/data_preprocessing_tools.py
@@ -8,11 +8,18 @@ import pandas as pd
 # Importing the dataset
 
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:, :-1].values # in order to take all row data -> [ : ,  all the columns except the last one -> :-1]
+X = dataset.iloc[:, :-1].values # in order to take all row data -> [ : ,  all the columns except the last one -> :-1]
 y = dataset.iloc[:, -1].values # all th rows and the last column
-print(x)
-print(y)
+
 # Taking care of missing data
+
+# missing data is replaced by average data of the column in which data is missing
+# np.nan -> blank cells
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
 # Encoding categorical data
 # Encoding the Independent Variable
 # Encoding the Dependent Variable
